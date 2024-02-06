aa84a3ccffd01a17d4d43f420d4642fe1256cc32
Dev Ops <Adrian.Diaconovici@telus.com>
2024-02-05 22:55:58 -0500

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://adriandia.github.io/themable/',
   plugins: [react()],
   resolve: {
     alias: {
